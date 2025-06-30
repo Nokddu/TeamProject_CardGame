14241dd7271313e294616ad407f5fbec66001f1d
Nokddu <sksksksk103@naver.com>
2025-06-30 20:22:01 +0900

    버튼 누르면 씬 이동하게

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class UIManager : MonoBehaviour
 {
     public Text Timetxt; // 위에 나오는 시간
@@ -31,25 +32,38 @@ public class UIManager : MonoBehaviour
 
 
 
-        End();
+        //if(time < 0f)
+        //{
+        //    End();
+        //}
     }
 
-    void End()
-    {
-        if(time < 0f)
-        {
-            Time.timeScale = 0;
-            EndPanel.SetActive(true);
-            GameEndTxt.gameObject.SetActive(true);
-            GameEndTxt.text = "시간이 다 되었습니다!!\n\n다시 도전?"; 
-        }
-        //if( 남은 생명 < 1이하면)
-        //{
-        //    Time.timeScale = 0;
-        //    EndPanel.SetActive(true);
-        //    GameEndTxt.gameObject.SetActive(true);
-        //    GameEndTxt.text = "\n기회를 전부 소진하였습니다 ㅠ^ㅠ";   // 게임 매니저에서 라이프 관리하는 변수를 가지고 와야됨
-        //}
+    //    void End()
+    //    {
+    //            Time.timeScale = 0;
+    //            EndPanel.SetActive(true);
+    //            GameEndTxt.gameObject.SetActive(true);
+    //            GameEndTxt.text = "시간이 다 되었습니다!!\n\n다시 도전?"; 
 
+    //       
+
+    //    }
+    //}
+    //    (남은 생명 < 1이하면)
+    //       
+    //            Time.timeScale = 0;
+    //          EndPanel.SetActive(true);
+    //          GameEndTxt.gameObject.SetActive(true);
+    //         GameEndTxt.text = "\n기회를 전부 소진하였습니다 ㅠ^ㅠ";   // 게임 매니저에서 라이프 관리하는 변수를 가지고 와야됨
+
+    public void Retry()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void EndButton()
+    {
+        SceneManager.LoadScene("EndScene");
     }
 }
+
