77c0a46c4630324ef8c6b9ce75579c349d29bdb6
Kazin7 <krustm8@naver.com>
2025-07-01 19:11:25 +0900

    delete unuseful data and change endPanel

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,17 +6,17 @@ using UnityEngine.SceneManagement;
 public class UIManager : MonoBehaviour
 {
     public Text Timetxt; // ���� ������ �ð�
-    public Text ClearTime; // Ŭ����� ���� �ð�
-    public Text GameEndTxt; // ������ �������� �̱���� ����� �� �Ȱ��� Ȥ�� �ð��� �� �Ȱ��� �����ִ� �ؽ�Ʈ
+    public GameObject normalClearEnd; //노말게임 성공시 나오는 사진과 글
+    public GameObject normalFailEnd;//노말게임 실패시 나오는 사진과 글
+    public GameObject timerGameEnd;//시간 제한모드 게임 종료시 나오는 글
     public GameObject EndPanel; // ������ ������ ������ �ǳ�
+    public string gameType = "Normal"; //타입 확인변수(나중에 게임매니저로 갈예정)
 
     private float time = 30f;
     private float Timer = 0f;
     void Start()
     {
         Timetxt.gameObject.GetComponent<Text>();
-        ClearTime.gameObject.GetComponent<Text>();
-        GameEndTxt.gameObject.GetComponent<Text>();
     }
 
     // Update is called once per frame
@@ -28,26 +28,31 @@ public class UIManager : MonoBehaviour
         float whitetored = Mathf.Lerp(1f, 0f, t);
         Timetxt.color = new Color(1f, whitetored, whitetored);
         Timetxt.text = time.ToString("N1");
-        ClearTime.text = time.ToString("N1");
 
 
 
-        //if(time < 0f)
-        //{
-        //    End();
-        //}
+        if(time < 0f)
+        {
+           End();
+        }
     }
 
-    //    void End()
-    //    {
-    //            Time.timeScale = 0;
-    //            EndPanel.SetActive(true);
-    //            GameEndTxt.gameObject.SetActive(true);
-    //            GameEndTxt.text = "�ð��� �� �Ǿ����ϴ�!!\n\n�ٽ� ����?"; 
-
-    //       
-
-    //    }
+       void End()
+       {
+        Time.timeScale = 0;
+        EndPanel.SetActive(true);
+        if (gameType == "Normal")
+        {
+            if (time > 0)
+            {
+                normalClearEnd.gameObject.SetActive(true);
+            }
+            else
+            {
+                normalFailEnd.gameObject.SetActive(true);
+            }
+        }
+       }
     //}
     //    (���� ���� < 1���ϸ�)
     //       
@@ -60,10 +65,14 @@ public class UIManager : MonoBehaviour
     {
         SceneManager.LoadScene("MainScene");
     }
-
-    public void EndButton()
+    public void MoveTitle()
     {
-        SceneManager.LoadScene("EndScene");
+        SceneManager.LoadScene("TitleScene");
+    }
+    public void EndButton() //실제로는 게임 종료되지만 화면상에는 안꺼짐
+    {
+        Debug.Log("게임 종료");
+        Application.Quit();
     }
 }
 
