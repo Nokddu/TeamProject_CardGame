05f15a8f562b08698f564fdb07d7a563503969d4
Nokddu <sksksksk103@naver.com>
2025-07-01 16:47:51 +0900

    Title
    연결 전

--- Assets/Scripts/StartButton.cs
+++ Assets/Scripts/StartButton.cs
@@ -5,19 +5,36 @@ using UnityEngine.SceneManagement;
 
 public class StartButton : MonoBehaviour
 {
-    public void GameStart()
+
+    // todo : 브금 나온거 꺼지게 혹은 켜지게
+
+    // todo : 시간제한모드 들어가기 Setbool? 잘 모르겠다.
+
+    public void IsTimed(bool isOn)
+    {
+        if(isOn)
+        {
+            // 시간 제한 모드
+        }
+        else
+        {
+            // 그냥 모드
+        }
+    }
+
+    public void StartBtn() // 메인 씬 이동하는 버튼
     {
         SceneManager.LoadScene("MainScene");
     }
 
-    // Start is called before the first frame update
-    void Start()
+    public void CollectionBtn() // 컬렉션 UI 버튼
     {
+        // 컬렉션 씬 열리게
+        //.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ExitBtn() // 게임 종료 버튼
     {
-        
+        Application.Quit();
     }
 }
